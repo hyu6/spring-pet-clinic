3eb2e119b28b41d5061a4338a2903e900a284bc7
Haoran Yu <haoranyu6@gmail.com>
2020-03-29 06:35:07 -0700

    create the project

---
+++ src/test/java/dev/hyux/springpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hyux.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
