487fdb248994add233e227b26cbdc0ab24cd11fb
Haoran Yu <haoranyu6@gmail.com>
2020-04-05 02:32:53 -0700

    refactor to common crud interface; close #11

--- pet-clinic-data/src/main/java/dev/hyux/springpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/dev/hyux/springpetclinic/services/PetService.java
@@ -2,13 +2,5 @@ package dev.hyux.springpetclinic.services;
 
 import dev.hyux.springpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
--- pet-clinic-data/src/main/java/dev/hyux/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/dev/hyux/springpetclinic/services/VetService.java
@@ -2,13 +2,5 @@ package dev.hyux.springpetclinic.services;
 
 import dev.hyux.springpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
