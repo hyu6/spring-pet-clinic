fe084b9732b9e94b8e5e7fceedd4b2e0c009ad69
Haoran Yu <haoranyu6@gmail.com>
2020-04-06 03:20:10 -0700

    update map impl to abstract id generation; close #23

--- pet-clinic-web/src/main/java/dev/hyux/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/dev/hyux/springpetclinic/bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Brown");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Alex");
         owner2.setLastName("Garcia");
 
@@ -38,14 +36,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Robert");
         vet1.setLastName("West");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Margaret");
         vet2.setLastName("Evans");
 
