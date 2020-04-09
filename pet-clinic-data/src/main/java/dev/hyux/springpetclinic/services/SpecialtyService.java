88eac4ee80125312d9bca4fc2fd1909f9e0a89a5
Haoran Yu <haoranyu6@gmail.com>
2020-04-09 05:11:11 -0700

    create and implement speciality map service; rename map services; close #31

---
+++ pet-clinic-data/src/main/java/dev/hyux/springpetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package dev.hyux.springpetclinic.services;
+
+import dev.hyux.springpetclinic.model.Speciality;
+
+public interface SpecialtyService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/dev/hyux/springpetclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,36 @@
+package dev.hyux.springpetclinic.services.map;
+
+import dev.hyux.springpetclinic.model.Speciality;
+import dev.hyux.springpetclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyMapService extends AbstractMapService<Speciality, Long> implements SpecialtyService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+}
